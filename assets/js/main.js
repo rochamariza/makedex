360e6dae06d9c3b114e7ed5802c074d97bd40c1a
Mariza <marizanfmrocha04@gmail.com>
2024-09-30 21:37:10 -0300

    Update 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log('Sucesso!')
+const offset = 0;
+const limit = 1;
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+fetch(url)
+    .then(function(response){
+        response.json().then(function(responseBody){
+            console.log(responseBody)
+        })
+    })
+    .then(function(jsonBody){
+        console.log(jsonBody)
+    })
+    .catch(function(error){
+        console.log(error)})
+    .finally(function(){
+        console.log("Requisição Concluída")
+    })
