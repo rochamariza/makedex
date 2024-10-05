89bec61bd2836d8337af75238af4dd2a639d74a4
Mariza <marizanfmrocha04@gmail.com>
2024-10-05 17:15:22 -0300

    Update 4

--- assets/js/main_alternativo.js
+++ assets/js/main_alternativo.js
@@ -9,27 +9,33 @@ const inputSearch = document.querySelector('#search')
 const fetchApiPokemon = async (pokemon) => {
 
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-
-    const data = await APIResponse.json()
-
-    return data;
+    if(APIResponse.status === 200){
+        const data = await APIResponse.json()
+        return data;
+    }
+    
 }
 
 const renderPoke = async (pokemon) => {
 
+    pokeName.innerHTML = 'Carregando...'
     const data = await fetchApiPokemon(pokemon);
 
-    cartao.style.display = "block";
+    if(data){
+        cartao.style.display = "block";
         pokeName.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1).toLowerCase()
         pokeID.innerHTML = `#${data.id}`
         statsPoke.innerHTML = data['types']['0']['type']['name']
         imagePoke.src = data['sprites']['other']['official-artwork']['front_default']
         inputSearch.value = '';
+    }
+    else{
+        cartao.style.display = "block";
+        pokeName.innerHTML = 'Error 404';
+    }
+    
 }
 form.addEventListener('submit', (event) => {
-
     event.preventDefault();
-    
     renderPoke(inputSearch.value.toLowerCase())
-
 })
